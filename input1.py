2bce4fea092b6d52bb79a033c4d19b11e2af43c7
nuriaale <112859130+nuriaale@users.noreply.github.com>
2023-06-19 17:20:51 +0200

    Update input1.py

--- input1.py
+++ input1.py
@@ -14,11 +14,8 @@ col_reaction='Unnamed: 0' #name of the colum that stores reaction ids
 medium_kpcs_file="inputs/kpc_cobas_biocrates(medium)_seahorse.xlsx"#Measured consumption and production rates. Metabolites being consumed should be negative, metabolites being produced should be positive
 colum_replicates=['KPC (umol/10e6 cell · h)']#name of the colum that indicates the condition of the replicates. 
 col_to_erase_l=['condition', 'Replicado', 'Unnamed: 0']
-#Metabolomics inputs
-max_NA_fraction_kpc=0.5 #if max_NA_fraction_kpc is 0.5, fluxes with more than 50% NA will be removed
-max_NA_fraction_pellet=0.5 #if max_NA_fraction_pellet is 0.5, metabolites with more than 50% NA will be removed
 
-only_run_MFA=False #Run simulation to test metabolimics. WILL NOT PRUNE THE MODELS IF SET TO TRUE
+
 
 #Gene expression inputs
 gene_expression_file=file_name="inputs/fpkm.xlsx"  #Absolute gene expression file, in FPKM or equivalent units
@@ -30,14 +27,8 @@ force_remove_th=1 #Genes with expression bellow 1 FPKM (absolute) will be remove
 objective="BIOMASS_reaction" #Objective reactions
 biomass_fraction=0.1 #Whats is the minimum fraction of bioamss allowed when removing genes 
 
-#Differential gene expression file
-deseq_file="inputs/dge.xlsx" #Differential gene expression
-#P values for differential expression
-comparisson_of_interest_deseq_pvalue={"CKpadj":"CK"}
-p_value_th=0.05 #P value to consider differential expression
 gene_entrez_column="entrezgene_id" #Name of the column with the entrez id
 #reference_condition="WT" #Control condition, used for other comparissons
-reference_condition=control_condition="CT" 
 
 #Reactions to keep and kill
 #Reactions that you dont want to be removed from the model
@@ -51,7 +42,4 @@ out_dir="OUTPUT" #Where the output files will be saved. Directory must exist
 
 #Check essential reactions using FVA
 fraction_essential_genes=0.1 #Reactions that must be active to allow growth above this fraction will be considered essential when looking for targets.
-
-#Find targets
-drug_information_file="inputs/drug_information.json" #File with annotations of known drugs and their targets. WARNING results should be manually checked as 
 
